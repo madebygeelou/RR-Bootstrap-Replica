4ad3e4b125f455f3a84d79aab889346d4b85bf51
madebygeelou <125709909+madebygeelou@users.noreply.github.com>
2024-04-30 13:07:08 -0700

    made my own variation of it since the guide had too many errors

--- src/App.js
+++ src/App.js
@@ -4,39 +4,54 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import TopBar from './components/TopBar.js'
-
+import MiddlePart from './components/MiddlePart.js';
+import Stack from 'react-bootstrap/Stack';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <TopBar/>
-     <h2>Browse Stores in Houston</h2>
-     <Container style = {{ marginTop: '40px', textAlign: 'center'}}>
-      <Row style={{borderBottom:'1px solid #E8E9EB', display: 'flex', justifyContent: 'space-evenly'}}>
-        <Row>
+      <MiddlePart>
+      </MiddlePart>
+      <div className='App'>
+     <h2>Browse Stores in Las Vegas</h2>    
+     <Container style={{ marginTop:'40px', textAlign: 'center'}}>
+     <Row style= {{borderBottom:'1px solid #E8E9EB', display: 'flex', justifyContent: 'space-evenly'}} xs={2} md={4} lg={6}>
           <Col xs='.5'>
             <img src='https://www.instacart.com/image-server/144x144/www.instacart.com/assets/domains/warehouse/logo/252/9921fd46-e9b5-4a9c-8598-dea54d037e3d.png' roundedCircle/>
-          </Col>
-          <Col>
-          <h5 style={{ fontWeight:'bold'}}>Smith's</h5>
+            <h5 style={{ fontWeight:'bold'}}>Smith's</h5>
           <p>Delivery * Pickup</p>
           </Col>
-        </Row>
-        <Col sm='1'></Col>
-        <Row>
           <Col xs='.5'>
             <img src='https://www.instacart.com/image-server/144x144/www.instacart.com/assets/domains/warehouse/logo/279/b6a0666c-e18e-4be3-a7a9-22def804a290.png' roundedCircle/>
+          <h5 style={{ fontWeight:'bold' }}>Sprouts Farmers Market</h5> 
           </Col>
-          <Col>
-          <h5 style={{ fontWeight:'bold' }}>Sprouts Farmers Market</h5>
-          </Col>
-          <Col ma='0'>
+          <Col xs='.5'>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjQy1BGnIkpuJVajWSm2gdoUkvf8Eykxb02_ManLYqdw&s' roundedCircle/>
             <h5 style={{ fontWeight: 'bold'}}>Kroger</h5>
             <p>Delivery</p>
           </Col>
-        </Row>
       </Row>
      </Container>
+     <Container style={{ marginTop:'40px', textAlign: 'center'}}>
+     <Row style= {{borderBottom:'1px solid #E8E9EB', display: 'flex', justifyContent: 'space-evenly'}} xs={2} md={4} lg={6}>
+          <Col xs='.5'>
+            <img src='https://www.instacart.com/image-server/144x144/www.instacart.com/assets/domains/warehouse/logo/252/9921fd46-e9b5-4a9c-8598-dea54d037e3d.png' roundedCircle/>
+            <h5 style={{ fontWeight:'bold'}}>Smith's</h5>
+          <p>Delivery * Pickup</p>
+          </Col>
+          <Col xs='.5'>
+            <img src='https://www.instacart.com/image-server/144x144/www.instacart.com/assets/domains/warehouse/logo/279/b6a0666c-e18e-4be3-a7a9-22def804a290.png' roundedCircle/>
+          <h5 style={{ fontWeight:'bold' }}>Sprouts Farmers Market</h5> 
+          </Col>
+          <Col xs='.5'>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjQy1BGnIkpuJVajWSm2gdoUkvf8Eykxb02_ManLYqdw&s' roundedCircle/>
+            <h5 style={{ fontWeight: 'bold'}}>Kroger</h5>
+            <p>Delivery</p>
+          </Col>
+      </Row>
+     </Container>
+     </div>
     </div>
   );
 }
--- src/components/MiddlePart.js
+++ src/components/MiddlePart.js
@@ -1,7 +1,24 @@
-
+import Image from 'react-bootstrap/Image';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
 
 function MiddlePart() {
-
+    return (
+        <Container className='form-container'>
+                    <h1 style={{fontWeight: 'bold'}}>Order groceries for delivery</h1>
+        <h1 style={{fontWeight: 'bold'}}>or pickup today</h1>
+        <p>Whatever you want from local stores, brought right to your door.</p>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicAddress">
+            <Form.Label>Enter your Address</Form.Label>
+            <Form.Control type="address" placeholder="Enter Address" />
+            <Form.Text className="text-muted">
+              We'll never share your address with anyone else.
+            </Form.Text>
+          </Form.Group>
+          </Form>
+          </Container>
+          )
 
 }
 
